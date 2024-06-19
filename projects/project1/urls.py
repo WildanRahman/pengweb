6b0e57dc7567948e40351d1e925d1747d18a0e2e
sebastian <raynosebastian19@gmail.com>
2024-06-19 10:14:52 +0700

    landing page

--- projects/project1/urls.py
+++ projects/project1/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from telcomedy import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('telcomedy/', include('telcomedy.urls')),
+    path('',include('telcomedy.urls')),
     
 ]
